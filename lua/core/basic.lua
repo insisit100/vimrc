05ac8846d70e180110b029a57d0c4eb7ef22bb05
insisit100 <hopmzsun@163.com>
2023-10-23 14:19:44 +0800

    Add files via upload

--- lua/core/basic.lua
+++ lua/core/basic.lua
@@ -1,1 +1,27 @@
+-- line number
+vim.opt.number = true
+vim.opt.relativenumber = true
 
+-- auto change directory
+vim.opt.autochdir = true
+
+-- highlight cursor line
+vim.opt.cursorline = true
+
+-- indent
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.autoindent = true
+
+
+vim.opt.listchars = {eol = '↲', tab = '▸ ', trail = '·'}
+-- enable mouse
+vim.opt.mouse = 'a'
+
+-- clipboard
+vim.cmd('set clipboard=unnamedplus')
+
+-- search ignorecase
+vim.opt.ignorecase = true
+vim.opt.hlsearch = false
